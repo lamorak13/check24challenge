9e5a16adbf921c6598a96cbab911960131e56cd6
Jakob Mayerhofer <Jakob.Mayerhofer@deutschebahn.com>
2024-05-25 20:13:16 +0200

    Feat: improve udpate query

--- backend/src/queries/UpdateRanking.sql
+++ backend/src/queries/UpdateRanking.sql
@@ -1,35 +1,26 @@
 with cp as (
     Select
-        (
-            (g. "awayscore" - g. "homescore") = (b. "awayscore" - b. "homescore")
-            and not(g. "homescore" = g. "awayscore")
-        ) as difference,
         Case
             when g. "awayscore" = b. "awayscore"
-            and g. "homescore" = b. "homescore" then true
-            else false
-        End as correct,
-        (
-            sign(g. "awayscore" - g. "homescore") = sign(b. "awayscore" - b. "homescore")
-        ) as tendency,
+            and g. "homescore" = b. "homescore" then 8
+            when (g. "awayscore" - g. "homescore") = (b. "awayscore" - b. "homescore")
+            and not(g. "homescore" = g. "awayscore") then 6
+            when sign(g. "awayscore" - g. "homescore") = sign(b. "awayscore" - b. "homescore") then 4
+            else 0
+        End as points,
         b. "userName" as name
     from
         "Bet" b,
         "Game" g
     where
-        g. "id" = uuid($1)
+        g. "id" = $1
         and b. "gameId" = g. "id"
 )
 Update
     "User" u
 set
-    points = u.points + Case
-        when cp.correct = true then 8
-        when cp.difference = true then 6
-        when cp.tendency = true then 4
-        else 0
-    End
-From
+    points = u. "points" + cp. "points"
+from
     cp
 where
     u. "name" = cp. "name"
--- backend/src/queries/UpdateRankings.ts
+++ backend/src/queries/UpdateRankings.ts
@@ -5,36 +5,27 @@ export const updateRankingsQuery = (gameId: string) =>
   prisma.$executeRaw(Prisma.sql`
     with cp as (
         Select
-            (
-                (g. "awayscore" - g. "homescore") = (b. "awayscore" - b. "homescore")
-                and not(g. "homescore" = g. "awayscore")
-            ) as difference,
             Case
                 when g. "awayscore" = b. "awayscore"
-                and g. "homescore" = b. "homescore" then true
-                else false
-            End as correct,
-            (
-                sign(g. "awayscore" - g. "homescore") = sign(b. "awayscore" - b. "homescore")
-            ) as tendency,
+                and g. "homescore" = b. "homescore" then 8
+                when (g. "awayscore" - g. "homescore") = (b. "awayscore" - b. "homescore")
+                and not(g. "homescore" = g. "awayscore") then 6
+                when sign(g. "awayscore" - g. "homescore") = sign(b. "awayscore" - b. "homescore") then 4
+                else 0
+            End as points,
             b. "userName" as name
         from
             "Bet" b,
             "Game" g
         where
-            g. "id" = uuid(${gameId})
+            g. "id" = ${gameId}
             and b. "gameId" = g. "id"
     )
     Update
         "User" u
     set
-        points = u.points + Case
-            when cp.correct = true then 8
-            when cp.difference = true then 6
-            when cp.tendency = true then 4
-            else 0
-        End
-    From
+        points = u. "points" + cp. "points"
+    from
         cp
     where
         u. "name" = cp. "name"
